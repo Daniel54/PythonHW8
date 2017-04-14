a75ebdba45b2ae565c93c090202765aefd8eac6a
Chris Langna <chris.langan195@gmail.com>
2017-04-14 16:04:12 -0600

    email portion of code working

--- email.sh
+++ email.sh
@@ -3,6 +3,9 @@
 #THIS PROGRAM CONTAINS ONLY EMAIL RELATED SCRIPTS
 #ALL CODE NEEDS TO BE MANUALLY INTEGRATED WITH THE MAIN SCRIPT 
 
+#This code relies on having sendmail installed. 
+#sudo apt-get install sendmail
+
 ./test.py #This needs to be changed to './create_report.py <args>' 
 ExitCode=$?	#grabs the exit code of the last run script
 Header="Error"
@@ -17,8 +20,7 @@ if [ $ExitCode = 0 ]
 	Header="Succesfully transfer file (FTP Address)" 
 	#don't forget to make it actually put the server name in
 
-	Body="Succesfully created a transaction report from (BegDate)
-	to (EndDate)"
+	Body="Succesfully created a transaction report from (BegDate) to (EndDate)"
 	#don't forget to make it actually put in the beg/end
 
 
@@ -38,6 +40,11 @@ else
 	echo "Check the exit codes of create_report.py"
 fi
 
-mail -s $Header chrislangan@mail.weber.edu <<< $Body #uses my email for testing
+touch email.txt
+echo "Subject: $Header" >> email.txt
+echo "$Body" >> email.txt
+
+sendmail chrislangan@mail.weber.edu < ./email.txt #Used my email for testing purposes. Replace it
+rm email.txt
 
 exit 0
