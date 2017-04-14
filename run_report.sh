d490abd903be9bf6595d4006e73131c72bb2b2d6
Daniel Lavulo <daniellavulo@mail.weber.edu>
2017-04-14 15:03:03 -0600

    Added in zip portion, fixed FTP functionality

--- run_report.sh
+++ run_report.sh
@@ -20,8 +20,6 @@ fi
 #Make a funciton that that uses BegDate and EndDate 
 #passes it to python script
 
-
-#looping for flags/ options -s -w -c
 while getopts ":f:t:e:u:p:" opt #once it finds it, it is stored in opt
 do 
 	case $opt in 
@@ -40,15 +38,70 @@ do
 			;;
 	esac
 
-
 done
 
-if [[ -z $size ]] #check if size is empty
-then
-	echo "s is required"
-	usage
-fi
+echo "Testing: [$BegDate] [$EndDate] [$email] [$user] [$passwd]"
+./test.py >> test4.dat #running py script
+################ENTER vm ip in var host####################################
+code=$?              #Capturing exit code from create_report.py
+host="137.190.19.97" #Capturing host variable will need to change.
+curDir=$PWD			 #Captur present working dir
+log=./ftplog.log     #ftp log file to check
+outFile="hw8Out"     #creating out file var
 
-echo "[$size] [$width] [$color]"
+
+
+
+
+
+if [[ $code == 0 ]]
+then
+    #############################################################
+    # zip portion
+	fileName=`find . -name '*.dat'` #getting file name
+	zipCall=`zip -v $outFile $fileName`
+
+	#Checking zip
+	if [[ $? -eq 1 ]]
+	then
+    	echo "zip failed"
+    	exit 1
+	else
+    	echo "good zip"
+	fi
+	#############################################################
+	#Ftp portion:
+
+	ftpFile=`find . -name '*.zip'`
+	
+	echo "Connecting to $user"
+    ftp -nv $host <<END_SCRIPT >$log
+	quote USER $user
+	quote PASS $passwd
+	put $ftpFile
+	quit
+END_SCRIPT
+	############################################################
+	#chekcing ftp
+	
+	echo "Disconnecting from $user"
+	echo "Emailing report to $email"
+
+	grep "226 Transfer complete" $log
+	if [[ $? == "1" ]]
+	then
+		echo "ftp failed"
+		exit 1
+	else
+		echo "ftp is complete"
+	fi
+elif [[ $code == 254 ]] #exit(-2)
+then
+	echo "Empty transaction: Send email"
+elif [[ $code == 255 ]] #exit(-1)
+then 
+	echo "Invalid input parameters."
+
+fi
 
 exit 0
