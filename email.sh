4b085bf101cdc880f12b05dc1b38b4e253f8f5c2
Chris Langna <chris.langan195@gmail.com>
2017-04-14 15:35:23 -0600

    email code updated

--- email.sh
+++ email.sh
@@ -5,18 +5,39 @@
 
 ./test.py #This needs to be changed to './create_report.py <args>' 
 ExitCode=$?	#grabs the exit code of the last run script
+Header="Error"
+Body="Error"
 
 if [ $ExitCode = 0 ]
 	then
-	echo "Exit code 0 returned"
+	zip report *.dat
+	
+	#Need to add in code to FTP the zip file to a server
+
+	Header="Succesfully transfer file (FTP Address)" 
+	#don't forget to make it actually put the server name in
+
+	Body="Succesfully created a transaction report from (BegDate)
+	to (EndDate)"
+	#don't forget to make it actually put in the beg/end
+
+
 elif [ $ExitCode = 255 ] #A returned -1 is seen as 255
 	then
-	echo "Exit code -1 returned"
+	Header="The create_report program exit with code -1"
+	Body="Bad input parameters (BegDate) (EndDate)"
+	#don't forget to make it acutally put in the beg/end date
+
 elif [ $ExitCode = 254 ] #A returned -2 is seen as 254
 	then
-	echo "Exit code -2 returned"
+	Header="The create_report program exit with code -2"
+	Body="No transactions available from (BegDate) to (EndDate)"
+	#don't forget to make it acutally put in the beg/end date
+
 else
 	echo "Check the exit codes of create_report.py"
 fi
+
+mail -s $Header chrislangan@mail.weber.edu <<< $Body #uses my email for testing
 
 exit 0
